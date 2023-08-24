a0b81d30d6aa80f5345cbabed3a59b8ac1a49206
mel-a-lab <melanie.dussenne@gmail.com>
2023-08-24 17:51:48 +0200

    autorisation

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -8,6 +8,7 @@ use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 class UserType extends AbstractType
 {
@@ -22,7 +23,14 @@ class UserType extends AbstractType
                 'first_options'  => ['label' => 'Mot de passe'],
                 'second_options' => ['label' => 'Tapez le mot de passe à nouveau'],
             ])
-            ->add('email', EmailType::class, ['label' => 'Adresse email']);
-           // ->add(''),  CheckboxType::class, []
+            ->add('email', EmailType::class, ['label' => 'Adresse email']) 
+            ->add('roles', ChoiceType::class, [
+                'multiple' => true,
+                'expanded' => true, 
+                'choices'  => [
+                    'Utilisateur' => 'ROLE_USER',
+                    'Administrateur' => 'ROLE_ADMIN',
+                ],
+            ]);
     }
 }
