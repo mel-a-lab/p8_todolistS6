02f8973fce3c3d026f114fca8c7a5b82828352c3
mel-a-lab <melanie.dussenne@gmail.com>
2023-09-05 16:58:58 +0200

    correction app_dev.php

--- public/app_dev.php
+++ public/app_dev.php
@@ -1,4 +1,3 @@
-
 <?php
 
 use Symfony\Component\HttpFoundation\Request;
@@ -10,10 +9,12 @@ use Symfony\Component\Debug\Debug;
 
 // This check prevents access to debug front controllers that are deployed by accident to production servers.
 // Feel free to remove this, extend it, or make something more sophisticated.
-if (isset($_SERVER['HTTP_CLIENT_IP'])
-    || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
-    || !(in_array(@$_SERVER['REMOTE_ADDR'], ['127.0.0.1', '::1']) || php_sapi_name() === 'cli-server')
-) {
+$request = Request::createFromGlobals();
+
+$clientIp = $request->getClientIp();
+$isLocalRequest = in_array($clientIp, ['127.0.0.1', '::1']) || php_sapi_name() === 'cli-server';
+
+if (!$isLocalRequest) {
     header('HTTP/1.0 403 Forbidden');
     exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
 }
@@ -24,10 +25,7 @@ Debug::enable();
 
 $kernel = new AppKernel('dev', true);
 $kernel->loadClassCache();
-
-// Utilisez Symfony pour créer la requête à partir des superglobales
-$request = Request::createFromGlobals();
-
 $response = $kernel->handle($request);
 $response->send();
 $kernel->terminate($request, $response);
+
