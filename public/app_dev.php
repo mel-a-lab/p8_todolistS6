2d52bae2f43f8b20080d248b0d5151b6be297ee1
mel-a-lab <melanie.dussenne@gmail.com>
2023-09-05 16:52:26 +0200

    app_dev.php

--- public/app_dev.php
+++ public/app_dev.php
@@ -6,7 +6,6 @@ use Symfony\Component\Debug\Debug;
 
 // If you don't want to setup permissions the proper way, just uncomment the following PHP line
 // read http://symfony.com/doc/current/book/installation.html#checking-symfony-application-configuration-and-setup
-// for more information
 //umask(0000);
 
 // This check prevents access to debug front controllers that are deployed by accident to production servers.
@@ -25,7 +24,10 @@ Debug::enable();
 
 $kernel = new AppKernel('dev', true);
 $kernel->loadClassCache();
+
+// Utilisez Symfony pour créer la requête à partir des superglobales
 $request = Request::createFromGlobals();
+
 $response = $kernel->handle($request);
 $response->send();
 $kernel->terminate($request, $response);
